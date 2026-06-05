2065715e3a7ac38f4033ea36e35baffe2b8db2ab
Sabnur Samiha <sabnursamiha630@gmail.com>
2026-06-05 14:00:44 +0530

    day 3 output add

---
+++ Week_01_C_Basics/Day_02_User_Input/Day_03_Control_Flow/02_max_of_three.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,p;
+    printf(" Enter three numbers :");
+    scanf("%d %d %d", &a,&b,&c);
+    if( a > b){
+        (p == a);
+    }
+    else if (a<b){
+        (p == b);
+
+    }
+    else if( p> c){
+        printf("The largest number is %d  ", p);
+    }
+    else{
+        printf( " The largest number is %d ", c);
+    }
+    
+    
+}
